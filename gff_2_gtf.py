92fb21009182e2f0ed11a585d186c763182c640d
AndrewLangvt <Al2025@wildcats.unh.edu>
2018-01-26 12:42:12 -0500

    Update gff_2_gtf.py

--- gff_2_gtf.py
+++ gff_2_gtf.py
@@ -16,51 +16,111 @@ def read_gff(filename):
 
         if bool(re.search("#.*", line)):
             continue
-        elif bool(re.search("region", column[2])):
-            continue
  
-        #only extract lines where the attribute is an exon
-        #search for "Genbank" and "transcript_id" in attributes, bordered 
-	#with a ";". If found, assign as "gen_id" "trans_id", respectively.
-
+        feature_line = ""
         attribute = str(column[2])
-        if attribute == "exon" : 
+
+
+
+        if attribute == "region" :
+           reg = re.search("ID=(.*?);", column[8])
+           if reg:
+               feature_line += 'ID "' + str(reg.group(1)) + '"; '
+
+           nam = re.search(".*Name=(.*?);", column[8])
+       	   if nam:
+               feature_line += 'Name "' + str(nam.group(1)) + '"; '
+
+           chrom = re.search(".*chromosome=(.*?);", column[8])
+       	   if chrom:
+               feature_line += 'Chromosome "' +	str(chrom.group(1)) + '"; '
+
+
+
+        elif attribute == "mRNA" :
+           mrna = re.search("ID=(.*?);", column[8])
+           if mrna:
+               feature_line += 'ID "' + str(mrna.group(1)) + '"; '
+
            gbank = re.search(".*GeneID:(.*?),", column[8])
            if gbank:
-               gen_id = gbank.group(1)
-           else:
-               gbank2 = re.search(".*GeneID:(.*)", column[8])
-               if gbank2:
-                   gen_id = gbank2.group(1)
-               else:
-                   continue
-#                   gen_id = ""
+               feature_line += 'gene_id "' + str(gbank.group(1)) + '"; '
+
+           nam = re.search(".*gene=(.*?);", column[8])
+           if nam:
+               feature_line += 'gene_name "' + str(nam.group(1)) + '"; '
 
            trans = re.search(".*transcript_id=(.*?);", column[8])
            if trans:
-               trans_id = trans.group(1)
+               feature_line += 'transcript_id "' + str(trans.group(1)) + '"; '
+           else:
+               trans2 = re.search(".*transcript_id=(.*)", column[8])
+               if trans2:
+                   feature_line += 'transcript_id "' + str(trans2.group(1)) + '"; '
+               else:
+                   continue
+
+
+
+        elif attribute == "exon" :
+           gbank = re.search(".*GeneID:(.*?),", column[8])
+           if gbank:
+               feature_line += 'gene_id "' + str(gbank.group(1)) + '"; '
+           else:
+               gbank2 = re.search(".*GeneID:(.*)", column[8])
+               if gbank2:
+                   feature_line += 'gene_id "' + str(gbank2.group(1)) + '"; '
+               else:
+                   continue
+
+           trans = re.search(".*transcript_id=(.*?);", column[8])
+           if trans:
+               feature_line += 'transcript_id "' + str(trans.group(1)) + '"; '
            else:
                trans2 = re.search(".*transcript_id=(.*)", column[8])
                if trans2: 
-                   trans_id = trans2.group(1)
+                   feature_line += 'transcript_id "' + str(trans2.group(1)) + '"; '
                else:
                    continue   
-#              	   trans_id = ""
+
+
+
+        elif attribute == "CDS" :
+           seq = re.search("ID=(.*?);", column[8])
+           if seq:
+               feature_line += 'ID "' + str(seq.group(1)) + '"; '
+           
+           parent = re.search(".*Parent=(.*?);", column[8])
+           if parent:
+               feature_line += 'ID "' + str(parent.group(1)) + '"; '
+
+           gbank = re.search(".*GeneID:(.*?),", column[8])
+           if gbank:
+               feature_line += 'gene_id "' + str(gbank.group(1)) + '"; '
+
+           prot = re.search(".*protein_id=(.*?);", column[8])
+           if prot:
+               feature_line += 'protein_id "' + str(prot.group(1)) + '"; '
+           else:
+               prot2 = re.search(".*protein_id=(.*)", column[8])
+               if prot2:
+                   feature_line += 'protein_id "' + str(prot2.group(1)) + '"; '
+               else:
+                   continue
+
+        else:
+            continue
             
 
 	#formatting gene_id and transcript_id
 
-           last_col = 'gene_id ' + '"' + gen_id + '"' + "; " + 'transcript_id ' + '"' + trans_id + '"' + ';'
-          
-           attribute = column[0] + "\t" + column[1] + "\t" + column[2] + "\t" + column[3] + "\t" + column[4] + "\t" + column[5] + "\t" + column[6] + "\t" + column[7] + "\t" + last_col + "\n"
-          
-           gtf += attribute
+        attribute = column[0] + "\t" + column[1] + "\t" + column[2] + "\t" + column[3] + "\t" + column[4] + "\t" + column[5] + "\t" + column[6] + "\t" + column[7] + "\t" + feature_line + "\n"
+        gtf += attribute
 
-        else:
-            continue   
 
     outfile.write(gtf)
 	
 if __name__ == '__main__':
     file = sys.argv[1]
     read_gff(file)
+
