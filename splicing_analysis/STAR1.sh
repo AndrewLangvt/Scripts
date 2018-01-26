2820ce66b21d880f1279dfc2c6da6aace96b0088
AndrewLangvt <Al2025@wildcats.unh.edu>
2018-01-26 12:36:39 -0500

    Create STAR1.sh

---
+++ splicing_analysis/STAR1.sh
@@ -0,0 +1,27 @@
+LOOK_LOC=$1
+
+for i in $(ls $LOOK_LOC/*.TRIM.R1.fq.gz); do
+
+        READ=$(basename -s .TRIM.R1.fq.gz $i)
+        STAR_OUT=${READ}_STAR_
+
+        echo ""
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        echo First round of mapping for $READ
+        echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        echo ""
+
+        STAR --runMode alignReads \
+        --genomeDir /pylon2/mc3bg6p/al2025/isoform/STAR_analysis/v1_STAR_indices \
+        --readFilesIn $LOOK_LOC/$READ.TRIM.R1.fq.gz \
+        $LOOK_LOC/$READ.TRIM.R2.fq.gz \
+        --sjdbGTFfile /pylon2/mc3bg6p/al2025/isoform/genome_compare/GCF_000337935.1_Cliv_1.0_genomic.gtf \
+        --sjdbOverhang 124 \
+        --outFilterScoreMinOverLread 0 \
+        --outFilterMatchNminOverLread 0 \
+        --outFilterMismatchNmax 2 \
+        --outSAMtype BAM SortedByCoordinate \
+        --readFilesCommand zcat \
+        --runThreadN 70 \
+        --outFileNamePrefix $STAR_OUT
+done
