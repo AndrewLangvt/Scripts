8edaeb509fb1be34ca3363760b218e4dad3dfc67
AndrewLangvt <Al2025@wildcats.unh.edu>
2018-01-29 09:47:14 -0500

    Update kallisto.sh

--- read_processing/kallisto.sh
+++ read_processing/kallisto.sh
@@ -5,5 +5,5 @@ ID_LIST=`ls -l *TRIM*gz | awk '{print $9}' | sed 's/.TRIM.*//g' | sort -u`
 for READ in $ID_LIST; do
 	kallisto quant -t 64 --bias -b 100 \
 -i /pylon2/mc3bg6p/al2025/mapping_indices/kallisto_HPG_parental_index.idx \
--o ${READ}_kallisto_out $READ.TRIM.*.R1.fq.gz $READ.TRIM.*.R2.fq.gz
+-o ${READ}_kallisto_out $READ.TRIM*.R1.fq.gz $READ.TRIM*.R2.fq.gz
 done
