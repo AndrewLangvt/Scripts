ed4984063f3f275be1c743d539acf934816a78d2
AndrewLangvt <Al2025@wildcats.unh.edu>
2018-02-08 11:51:40 -0500

    Create salmon_single.sh

---
+++ read_processing/salmon_single.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script will map a single readset with Salmon.
+
+DATA=$1
+SAMPLE=$2 
+#In the shell script execution, include the following
+# First cmd line arg = name for overarching salmon file
+# 2nd cmd line arg = name of sample
+
+PIGEON_ASSEMBLY=/pylon2/mc3bg6p/al2025/parental_assembly/RockDove.HPG.v1.1.1.fasta
+SALMON_DIR=/pylon2/mc3bg6p/al2025/storage/salmon-0.8.2/bin
+SALMON_INDEX=/pylon2/mc3bg6p/al2025/mapping_indices/salmon_index/HPG_parental_index
+
+# If index is previously generated, the script can access the index file variable $SALMON_INDEX
+# defined above.  If index needs to be generated, uncomment the below command to generate
+# an index file for the desired assembly.
+
+#$SALMON_DIR/salmon index --type quasi --threads 28 --index ${DATA}_index -t $PIGEON_ASSEMBLY
+
+SALMON_OUT=${DATA}_salmon_mapping
+
+OUTFILE_SALMON=${SAMPLE}_maps.txt
+mkdir ${SALMON_OUT}/salmon_$SAMPLE
+
+$SALMON_DIR/salmon quant -i $SALMON_INDEX -l A \
+-1 $SAMPLE.TRIM*.R1.fq.gz \ 
+-2 $SAMPLE.TRIM*.R2.fq.gz \
+-p 32 --gcBias --seqBias --output ${SALMON_OUT}/salmon_${SAMPLE}/$SAMPLE 2>&1 | tee -a $OUTFILE_SALMON 	      
+
+mv $OUTFILE_SALMON ${SALMON_OUT} 
+
