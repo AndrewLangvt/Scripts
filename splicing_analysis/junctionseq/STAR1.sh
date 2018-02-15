03cb4b653ec28023e2e5aff55252e6b10f055f3b
AndrewLangvt <Al2025@wildcats.unh.edu>
2018-02-15 10:54:37 -0500

    Update STAR1.sh

--- splicing_analysis/junctionseq/STAR1.sh
+++ splicing_analysis/junctionseq/STAR1.sh
@@ -12,10 +12,10 @@ for i in $(ls $LOOK_LOC/*.TRIM.R1.fq.gz); do
         echo ""
 
         STAR --runMode alignReads \
-        --genomeDir /pylon2/mc3bg6p/al2025/isoform/STAR_analysis/v1_STAR_indices \
+        --genomeDir /pylon2/mc3bg6p/al2025/isoform/STAR_analysis/v2_STAR_indices \
         --readFilesIn $LOOK_LOC/$READ.TRIM.R1.fq.gz \
         $LOOK_LOC/$READ.TRIM.R2.fq.gz \
-        --sjdbGTFfile /pylon2/mc3bg6p/al2025/isoform/genome_compare/GCF_000337935.1_Cliv_1.0_genomic.gtf \
+        --sjdbGTFfile /pylon2/mc3bg6p/al2025/isoform/colLiv2_genome/Rockdove_cliv2.gtf \
         --sjdbOverhang 124 \
         --outFilterScoreMinOverLread 0 \
         --outFilterMatchNminOverLread 0 \
