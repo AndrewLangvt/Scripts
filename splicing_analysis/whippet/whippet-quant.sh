ad63f23e6aadd570f225063931b6c1dc6b0369a7
AndrewLangvt <Al2025@wildcats.unh.edu>
2018-02-15 10:24:24 -0500

    Update whippet-quant.sh

--- splicing_analysis/whippet/whippet-quant.sh
+++ splicing_analysis/whippet/whippet-quant.sh
@@ -3,9 +3,9 @@ cd /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/
 READ_LOC=/pylon2/mc3bg6p/al2025/isoform/subset
 
 ##julia /opt/packages/whippet/Whippet.jl-0.8/bin/whippet-index.jl \
-##--fasta /pylon2/mc3bg6p/al2025/isoform/genome_compare/GCF_000337935.1_Cliv_1.0_genomic.fna \
-##--gtf /pylon2/mc3bg6p/al2025/isoform/genome_compare/GCF_000337935.1_Cliv_1.0_genomic.gtf \
-##--index /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/index/ColLiv_v1
+##--fasta /pylon2/mc3bg6p/al2025/isoform/colLiv2_genome/Rockdove_cliv2.gtf
+##--gtf /pylon2/mc3bg6p/al2025/isoform/colLiv2_genome/Rockdove_cliv2.gtf
+##--index /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/index/ColLiv_v2
 
 for i in $(ls $READ_LOC/*TRIM.R1.fq.gz); do
 
@@ -14,7 +14,7 @@ for i in $(ls $READ_LOC/*TRIM.R1.fq.gz); do
         echo mapping $SAMPLE
         echo ""
         julia /opt/packages/whippet/Whippet.jl-0.8/bin/whippet-quant.jl \
-        --index /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/index/ColLiv_v1 \
+        --index /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/index/ColLiv_v2 \
         --out /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/quant/$SAMPLE \
         --mismatches 2 \
         --score-min 0 \
