35b7443f0724901534db23ddbf565c803b085d5d
AndrewLangvt <Al2025@wildcats.unh.edu>
2018-02-21 15:21:11 -0500

    Update and rename v2_STAR_index.sh to v2_STAR_index.job

--- splicing_analysis/junctionseq/v2_STAR_index.sh
+++
@@ -1,6 +0,0 @@
-mkdir /pylon2/mc3bg6p/al2025/isoform/STAR_analysis/v2_STAR_indices
-
-STAR --runMode genomeGenerate --genomeDir /pylon2/mc3bg6p/al2025/isoform/STAR_analysis/v2_STAR_indices \
---genomeFastaFiles /pylon2/mc3bg6p/al2025/isoform/colLiv2_genome/GCA_001887795.1_colLiv2_genomic.fna \
---sjdbGTFfile /pylon2/mc3bg6p/al2025/isoform/colLiv2_genome/Rockdove_cliv2.gtf \
---sjdbOverhang 124
