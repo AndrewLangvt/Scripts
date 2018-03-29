a3ae6675be7b3d3896290dbdf89b705f6e227663
AndrewLangvt <Al2025@wildcats.unh.edu>
2018-03-29 09:28:48 -0400

    Update whippet_delta.sh

--- splicing_analysis/whippet/whippet_delta.sh
+++ splicing_analysis/whippet/whippet_delta.sh
@@ -8,16 +8,15 @@ TREATMENT2=$2
 SET_A=""
 SET_B=""
 
-for i in $(ls /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/quant/*_${TREATMENT1}*.psi.gz); do
+for i in $(ls /pylon5/mc3bg6p/al2025/isoform/whippet_analysis/quant/*_${TREATMENT1}*.psi.gz); do
         SET_A+=$i,
 done
 
-for i in $(ls /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/quant/*_${TREATMENT2}*.psi.gz); do
+for i in $(ls /pylon5/mc3bg6p/al2025/isoform/whippet_analysis/quant/*_${TREATMENT2}*.psi.gz); do
         SET_B+=$i,
 done
 
-julia /opt/packages/whippet/Whippet.jl-0.8/bin/whippet-delta.jl \
+julia /opt/packages/whippet/julia-0.6.0/v0.6/Whippet/bin/whippet-delta.jl \
 -a $SET_A \
 -b $SET_B \
--d /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/quant \
--o /pylon2/mc3bg6p/al2025/isoform/whippet_analysis/diff_files/${TREATMENT1}_${TREATMENT2}_comparison
+-o /pylon5/mc3bg6p/al2025/isoform/whippet_analysis/diff_files/${TREATMENT1}_${TREATMENT2}_comparison
