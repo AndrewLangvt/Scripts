9edb3ef6cb1ce6341b37b8ffd095b47e9105befa
AndrewLangvt <Al2025@wildcats.unh.edu>
2018-04-02 11:29:43 -0400

    Update sig_events_genes.py

--- splicing_analysis/whippet/sig_events_genes.py
+++ splicing_analysis/whippet/sig_events_genes.py
@@ -1,7 +1,7 @@
 # sig_events_genes.py
 # Andrew S. Lang
 # Created: 23MAR2018
-# Last Modified: 26MAR2018
+# Last Modified: 02APR2018
 
 # This script will take an input diff.gz file from whippet, and write all of the genes in which significant 
 # splicing events (prob > 0.95) occur
@@ -25,20 +25,17 @@ def sig_ev(fname):
             geneID = str(column[0])
             prob = float(column[8])
             psi = abs(float(column[7]))
+            event = str(column[2])
 
             if prob >= float(0.95) :
-#                if psi >= float(0.1) :
-                    if geneID not in spliced_genes_list:
-                        spliced_genes_list.append(geneID)
-#                else:
-#                    continue
+                if psi >= float(0.1) :
+                    spliced_genes+= geneID + "\t" + str(psi) + "\t" + event + "\n"
+                else:
+                    continue
             else:
                 continue
 
-        for gene in spliced_genes_list:
-            spliced_genes += gene + "\n"
-
-        outfile.write("Analysis:Whippet " + name_trimmed + "\n" + spliced_genes)
+        outfile.write("Analysis:Whippet " + name_trimmed + "Event_Loci" + "\n" + spliced_genes)
         outfile.close()
 
 if __name__ == '__main__':
